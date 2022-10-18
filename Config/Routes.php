63e68c9a00f5c071355eb75db143040c79bbef73
flouvat <s.flouvat@agencelafusee.com>
2022-10-18 21:08:41 +0200

    route securisation

--- Config/Routes.php
+++ Config/Routes.php
@@ -8,18 +8,18 @@ class Routes{
   public static function getRoutes(){
     return [
       // hompeage
-      "" => ["controller" => "home", "action" => "index", "role" => ["public, admin"]],
-      "home" => ["controller" => "home", "action" => "index", "role" => ["public, admin"]],
-      "resume" => ["controller" => "home", "action" => "resume", "role" => ["public, admin"]],
+      "" => ["controller" => "home", "action" => "index", "role" => ["public", "admin"]],
+      "home" => ["controller" => "home", "action" => "index", "role" => ["public", "admin"]],
+      "resume" => ["controller" => "home", "action" => "resume", "role" => ["public", "admin"]],
 
       // signin/signup
-      "inscription" => ["controller" => "registration", "action" => "index", "role" => ["public, admin"]],
-      "connexion" => ["controller" => "login", "action" => "index", "role" => ["public, admin"]],
-      "logout" => ["controller" => "login", "action" => "logout", "role" => ["user, admin"]],
+      "inscription" => ["controller" => "registration", "action" => "index", "role" => ["public", "admin"]],
+      "connexion" => ["controller" => "login", "action" => "index", "role" => ["public", "admin"]],
+      "logout" => ["controller" => "login", "action" => "logout", "role" => ["user", "admin"]],
 
       // posts
-      "posts" => ["controller" => "post", "action" => "index", "role" => ["public, admin"]],
-      "post" => ["controller" => "post", "action" => "show", "role" => ["public, admin"]],
+      "posts" => ["controller" => "post", "action" => "index", "role" => ["public", "admin"]],
+      "post" => ["controller" => "post", "action" => "show", "role" => ["public", "admin"]],
 
       // admin
       "connexion-admin" => ["controller" => "admin", "action" => "connexion", "role" => ["admin"]],
